6c52faf661e0d2f036f4fec0349b04df3c9681e1
Roy_computer <roy_computer@Roy-computerdeMacBook-Pro.local>
2019-09-23 13:58:30 +0800

    没有ajax的，只是数据逻辑正确

---
+++ 1登录注册/back/1-index.js
@@ -0,0 +1,58 @@
+const http = require('http')
+const url = require('url')
+const querystring = require('querystring')
+let userDB = {hu:123}
+let server = http.createServer((req,res)=>{
+  let path,username,password;
+  if(req.method == "GET"){
+    let {pathname,query} = url.parse(req.url,true)
+    path = pathname
+    username = query.username
+    password = query.password
+    complete(path,username,password);
+  }
+
+  function complete(path,username,password){
+    if(path == "/login"){
+      let msg;
+      if(!userDB[username]){
+        msg = "no"
+        res.write(JSON.stringify({error:1,msg}))
+        res.end()
+      }else{
+        if(userDB[username]!=password){
+          msg = "incorrect"
+          res.write(JSON.stringify({error:1,msg}))
+          res.end()
+        }else{
+          msg = "success"
+          res.write(JSON.stringify({error:0,msg}))
+          res.end()
+        }
+
+      }
+    }else if(path == "/reg"){
+      let msg;
+      console.log(userDB[username])
+      if(!userDB[username]){
+        if(username==""||password==""){
+          msg = "toFull"
+          res.write(JSON.stringify({error:1,msg}))
+          res.end()
+        }else{
+          userDB[username] = password;
+          msg = "reg success"
+          res.write(JSON.stringify({error:0,msg}))
+          res.end()
+        }
+      }else{
+        msg = "already"
+        res.write(JSON.stringify({error:1,msg}))
+        res.end()
+      }
+    }
+  }
+})
+server.listen(3000,()=>{
+  console.log("server on 3000")
+})
